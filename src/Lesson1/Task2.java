000d467102c32c89632a2721c461a0382f878c19
Anastasia <anastasia.zevakina@gmail.com>
2020-12-03 14:09:35 +0300

    7

---
+++ src/Lesson1/Task2.java
@@ -0,0 +1,22 @@
+package Lesson1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 10;
+        System.out.println(myMath(a, b));
+        boolean less = myMath(a, b) < 10;
+        boolean more = myMath(a, b) > 20;
+
+        if (less) {
+            System.out.println("false");
+        } else if (more) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+        }
+    public static int myMath(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/Lesson1/Task4.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String name = "Anastasia";
+        System.out.println("Have a good day, " + name);
+    }
+}
