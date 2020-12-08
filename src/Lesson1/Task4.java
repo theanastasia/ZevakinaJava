e5e74a3fb3d4b4d30a1723116eadb037dbe04624
Anastasia <anastasia.zevakina@gmail.com>
2020-12-08 14:17:24 +0300

    2

--- src/Lesson1/Task4.java
+++
@@ -1,8 +0,0 @@
-package Lesson1;
-
-public class Task4 {
-    public static void main(String[] args) {
-        String name = "Anastasia";
-        System.out.println("Have a good day, " + name);
-    }
-}
