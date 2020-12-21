86571f42b53876f5c64c7a7062b4815d72dd3a21
Anastasia <anastasia.zevakina@gmail.com>
2020-12-21 19:43:58 +0300

    Home Work NUMBER 5

--- lesson5/zoo/Dog.java
+++
@@ -1,9 +0,0 @@
-package lesson5.zoo;
-
-import lesson5.Animal;
-
-public class Dog extends Animal {
-    public Dog(String type, String name, float run, float swim, float jump) {
-        super(type, name, run, swim, jump);
-    }
-}
--- lesson5/zoo/Horse.java
+++
@@ -1,9 +0,0 @@
-package lesson5.zoo;
-
-import lesson5.Animal;
-
-public class Horse extends Animal {
-    public Horse(String type, String name, float run, float swim, float jump) {
-        super(type, name, run, swim, jump);
-    }
-}
